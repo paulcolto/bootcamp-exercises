7c173ca1248f2497f80fc3b5179b3e4524a73973
Paul Coltofeanu <paulcolto@gmail.com>
2019-09-26 14:42:26 +0200

    Rolling a dice JS exercise

---
+++ day12-javascript/js demo/index.js
@@ -0,0 +1,62 @@
+/* SALARY */
+
+/* const wage = 350;
+const salary = wage * 7 * 20;
+console.log(salary); */
+
+/* CURRENCY EXCHANGE  */
+
+/* const wage = prompt('Enter your hourly wage:');
+const hours = prompt('How many hours do you work each day?');
+const days = prompt('How many days do you work each month?')
+document.write('You earn ' + (wage * hours * days) + ' euros every month.')
+document.write(' That is ' + Math.round((wage * hours * days) * 25.695) + ' czk per month.' ) */
+
+/* WORKING HOURS */
+
+/* let hours = [6, 7, 6, 8, 9, 5, 6];
+console.log('On Sunday, you worked for ' + hours[6] + ' hours.');
+
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+days.push('Saturday', 'Sunday');
+console.log(days[0]);
+console.log(days[2]);
+console.log(days[5]);
+console.log(days.length); */
+
+/* CHESS BOARD */
+
+/* let chessPosition = [
+    [5, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 1, 3],
+    [0, 0, 0, 0, 0, 0, 1, 6],
+    [0, 0, 0, 0, 3, 0, 2, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 6, 0]
+];
+
+chessPosition[4][7] = 0;
+chessPosition[1][2] = 6;
+
+console.log(chessPosition); */
+
+/* RANDOM NUMBERS (not working) */
+
+/* const min = prompt('Choose a minimum number?');
+const max = prompt('Now, choose a maximum number');
+
+const randomNumber = (Math.floor(Math.random(min + max) + 1));
+document.write(randomNumber);
+ */
+
+
+
+
+
+
+
+
+
+
---
+++ day13-more-javascript/dist/js/index.js
@@ -0,0 +1,86 @@
+/* const accountOne = 000256784;
+const accountTwo = 000986056;
+const accountThree = 001546238;
+const accountFour = 030987456;
+
+const balanceOne = 790;
+const balanceTwo = 5600;
+const balanceThree = 78065;
+const balanceFour = 120910;
+
+const enteredAccountNumber = prompt('Please enter your account number');
+
+if (enteredAccountNumber = accountOne) {
+    document.write(balanceOne);
+} else if (enteredAccountNumber = accountTwo) {
+        document.write(balanceTwo);
+    } else if (enteredAccountNumber = accountThree) {
+        document.write(balanceThree);
+    } else if (enteredAccountNumber = accountFour) {
+        document.write(balanceFour)
+    } else {
+        document.write('Your account number is invalid.')
+    }
+ */
+
+ /* ENTER PASSWORD AND USERNAME */
+
+ /* const username = prompt("Please enter your username");
+ const password = prompt("Now please enter your password");
+
+ if (password === 'sore-thumb-2018') {
+     document.write('Access granted!');
+ } else {
+     document.write('Nice try.');
+ } */
+
+/* PASSWORD VERIFICATION CHECK */
+
+/*  const password = prompt("Please choose a password");
+ const verifyPassword = prompt('Please re-enter your password');
+
+ if (verifyPassword.length < 8) {
+    document.write('Your password is not secure.');
+} else if (password === verifyPassword) {
+     document.write('Password verification confirmed');
+ } else {
+     document.write('Your passwords do not match');
+ } */
+
+ /* BUS TICKET PRICES */
+ 
+/* const age = prompt('Please enter your age');
+const ticketPrice = 12;
+
+if (age < 6) {
+    alert('Your ticket price is 0 euro')
+} else if (age < 15) {
+    alert('Your ticket price with pupil discount is: ' + Math.floor(ticketPrice * 0.35) + ' euros.');
+} else if (age < 26) {
+    alert('Your ticket price with student discount is: ' + Math.floor(ticketPrice * 0.65) + ' euros.');
+} else if (age < 64) {
+    alert('Your ticket price is: ' + ticketPrice + ' euros.');
+} else if (age >= 64) {
+    alert('Your ticket price with seniors discount is: ' + Math.floor(ticketPrice * 0.5) + ' euros.');
+};
+ */
+/* 
+ // Mathematical Functions
+ 
+ const square = (a) => {
+     return a * a; 
+ };
+
+ const hypotenuse = (a, b) => {
+     return Math.sqrt(square(a) + square(b));
+ }
+
+ // Salary Function
+
+ const salary = (pay, hours, days) => {
+     return pay * hours * days; 
+ }
+
+ const taxedSalary = (sal, tax) => {
+     return sal * (1 - tax);
+ } */
