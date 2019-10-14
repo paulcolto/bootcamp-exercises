7fb3f30f74bff21fe6e05010b6ace24aa192ab06
Paul Coltofeanu <paulcolto@gmail.com>
2019-10-14 12:49:05 +0200

    php-oop-react

---
+++ day26-php-oop-react/morning-workout/src/Celebrity/Celebrity.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Celebrity extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div className="celebrity">
+                
+            </div>
+        );
+    }
+}
