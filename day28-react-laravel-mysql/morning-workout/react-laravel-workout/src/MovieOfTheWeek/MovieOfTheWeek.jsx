6b83f1bc2eac7b9a6fdd517f560839d7e5b8ec81
Paul Coltofeanu <paulcolto@gmail.com>
2019-10-17 13:01:12 +0200

    react laravel and mysql

---
+++ day28-react-laravel-mysql/morning-workout/react-laravel-workout/src/MovieOfTheWeek/MovieOfTheWeek.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+export default class MovieOfTheWeek extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            loaded: false,
+            data: []
+        };
+
+        this.url = 'http://www.laravel.test:8081/api/movie_of_the_week';
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData() {
+        if (this.url) {
+            this.setState({ 
+                loading: true,
+                loaded: false,
+                data: []
+            })
+
+            fetch(this.url)
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({ 
+                        loaded: true,
+                        data: data
+                    })
+                })
+                .finally(this.setState({
+                    loading: false
+                }));
+        }
+    }
+
+    render() {
+
+        let content = (
+            <div className="message">
+                <div className="loader"><div></div><div></div><div></div><div></div></div>
+                Loading
+            </div>
+        )
+        if (!this.state.loading && this.state.loaded) {
+            content = (
+                <>
+                    <div className="movie">
+
+                        <img src="https://m.media-amazon.com/images/M/MV5BNzAwNzUzNjY4MV5BMl5BanBnXkFtZTgwMTQ5MzM0NjM@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="Venom" />
+
+                        <div>
+
+                            <h3>Venom</h3>
+
+                            <div className="year">2018</div>
+
+                            <div className="genres">
+                                <span>Action</span>
+                                <span>Horror</span>
+                                <span>Sci-Fi</span>
+                            </div>
+
+                            <div className="starring">
+                                <h4>Starring:</h4>
+
+                                <div>
+                                    <span>Tom Hardy</span>
+                                    <span>Michelle Williams</span>
+                                    <span>Riz Ahmed</span>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </>
+            )
+        }
+        
+        return (
+            <section className="weekly-movie">
+
+                <h2>Movie of the week</h2>
+
+                { content }
+
+            </section>
+        );
+    }
+}
